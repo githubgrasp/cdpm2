38e633bd86998991ecf30d586da733fb9eb2d698
githubgrasp <peter.grassl@glasgow.ac.uk>
2019-11-10 23:32:33 +0000

    merge license and fortran file

--- cdpm2umat.f
+++ cdpm2umat.f
@@ -1,4 +1,28 @@
+c*****************************************************************
 c
+c Copyright (c) [2019] [Peter Grassl]
+c
+c Permission is hereby granted, free of charge, to any person obtaining a copy
+c of this software and associated documentation files (the "Software"), to deal
+c in the Software without restriction, including without limitation the rights
+c to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+c copies of the Software, and to permit persons to whom the Software is
+c furnished to do so, subject to the following conditions:
+c
+c The above copyright notice and this permission notice shall be included in all
+c copies or substantial portions of the Software.
+c
+c THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+c IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+c FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+c AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+c LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+c OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+c SOFTWARE.
+c**********************************************************************
+c
+c
+      
       Subroutine umat50v(cm,d1,d2,d3,d4,d5,d6,sig1,sig2,
      . sig3,sig4,sig5,sig6,epsps,hsvs,lft,llt,dt1siz,capa,
      . etype,tt,temps,failels,nlqa,crv,cma,qmat,elsizv,idelev)
